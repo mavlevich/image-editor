28afd65e3e8ce6868095eda7bb57bce4132cd87d
mavlevich <mavlevichx@gmail.com>
2023-12-01 00:17:43 +0100

    add save function

--- filter_pixel.py
+++ filter_pixel.py
@@ -11,10 +11,13 @@ def pixelate_image(image, block_size):
     small_image = cv2.resize(image, (block_size, block_size), interpolation=cv2.INTER_LINEAR)
 
     # Resize the small image back to the original size
-    photo_filter_pixel = cv2.resize(small_image, (width, height), interpolation=cv2.INTER_NEAREST)
+    image_filter_pixel = cv2.resize(small_image, (width, height), interpolation=cv2.INTER_NEAREST)
+
+    # Save the pixelated image
+    cv2.imwrite('image_filter_pixel.jpg', image_filter_pixel)
 
     # Show the pixelated image
-    cv2.imshow('photo_filter_pixel', photo_filter_pixel)
+    cv2.imshow('image_filter_pixel', image_filter_pixel)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
