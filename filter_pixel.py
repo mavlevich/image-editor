9a824d41723e5dda3925ba6ff191c9eea90e55ac
mavlevich <mavlevichx@gmail.com>
2023-11-30 23:56:27 +0100

    Initial commit

---
+++ filter_pixel.py
@@ -0,0 +1,23 @@
+import cv2
+
+
+def pixelate_image(image, block_size):
+    image = cv2.imread(image)
+
+    # Get the dimensions of the image
+    height, width = image.shape[:2]
+
+    # Resize the image to a smaller size
+    small_image = cv2.resize(image, (block_size, block_size), interpolation=cv2.INTER_LINEAR)
+
+    # Resize the small image back to the original size
+    photo_filter_pixel = cv2.resize(small_image, (width, height), interpolation=cv2.INTER_NEAREST)
+
+    # Show the pixelated image
+    cv2.imshow('photo_filter_pixel', photo_filter_pixel)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+pixelate_image('image.jpg', 32)
+
